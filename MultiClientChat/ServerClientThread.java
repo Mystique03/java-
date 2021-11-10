4812fddd14be5e95f6b7bae3060e95b103140375
Smitha Reddy <89924555+Mystique03@users.noreply.github.com>
2021-11-10 10:22:15 +0530

    Add files via upload

---
+++ MultiClientChat/ServerClientThread.java
@@ -0,0 +1,36 @@
+import java.net.*;
+import java.io.*;
+ 
+class ServerClientThread extends Thread {
+  Socket serverClient;
+  int clientNo;
+  int squre;
+  ServerClientThread(Socket inSocket,int counter){
+    serverClient = inSocket;
+    clientNo=counter;
+  }
+  public void run(){
+    try{
+      DataInputStream inStream = new DataInputStream(serverClient.getInputStream());
+      DataOutputStream outStream = new DataOutputStream(serverClient.getOutputStream());
+      BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+      String clientMessage="", serverMessage="";
+      while(!clientMessage.equals("bye")){
+        clientMessage=inStream.readUTF();
+        System.out.println("From Client-" +clientNo+ ":"+clientMessage);
+        System.out.println("Enter the Message:");
+        serverMessage=br.readLine();
+        outStream.writeUTF(serverMessage);
+        //outStream.flush();
+      }
+      inStream.close();
+      outStream.close();
+      serverClient.close();
+    }catch(Exception ex){
+      System.out.println(ex);
+    }finally{
+      System.out.println("Client -" + clientNo + " exit!! ");
+    }
+  }
+}
+
